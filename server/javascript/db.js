0e7deb3f0f892bd164c1058b3fc55988ad368963
pfannkuchen <nathan.pioucraft@outlook.com>
2023-09-18 10:46:13 +0200

    Technically, I finished the signup thing.

---
+++ server/javascript/db.js
@@ -0,0 +1,17 @@
+const pg = require("pg")
+
+const client = new pg.Client({database: "test", user: "postgres"})
+client.connect().then(() => console.log("connected to the database"))
+
+async function query(query, parameters) {
+    let res
+    if(parameters) {
+        res = await client.query(query, parameters)
+    }
+    else {
+        res = await client.query(query)
+    }
+    return res
+}
+
+module.exports = query
--- server/routes/api/createAccount.js
+++ server/routes/api/createAccount.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const pg = require("pg")
+const query = require("./../../javascript/db")
+const crpto = require("crypto")
+const forge = require('node-forge');
 
 const router = express.Router()
 const jsonMiddleware = bodyParser.json({ type: 'application/json' });
 router.use(jsonMiddleware)
-
-const client = new pg.Client({database: "test", user: "postgres"})
-client.connect().then(() => console.log("connected to the database"))
 
 
 router.post("/", async (req, res) => {
@@ -22,14 +21,15 @@ router.post("/", async (req, res) => {
                 res.status(400)
             }
             else {
-                if((await client.query("SELECT * FROM Users WHERE handle ~* $1;", [handle])).rowCount != 0) {
+                if((await query("SELECT * FROM Users WHERE handle ~* $1;", [handle])).rowCount != 0) {
                     res.status(409).send({error: "handle already taken"})
                 }
-                else if((await client.query("SELECT * FROM Users WHERE email ~* $1;", [email])).rowCount != 0) {
+                else if((await query("SELECT * FROM Users WHERE email ~* $1;", [email])).rowCount != 0) {
                     res.status(409).send({error: "email already taken"})
                 }
                 else {
-                    //create a file for all the db requests
+                    let hash = await Bun.password.hash(password);
+                    query("INSERT INTO Users (handle, username, email, password, emailVerification) VALUES ($1, $2, $3, $4, $5);", [handle, username, email, hash, crypto.randomUUID()])
                     res.send("sucess")
                 }
             }
@@ -43,5 +43,18 @@ router.post("/", async (req, res) => {
         console.log(err)
     }
 })
+
+
+async function generateKeyPair() {
+    // Create an RSA key pair
+    const keyPair = forge.pki.rsa.generateKeyPair({ bits: 2048 });
+
+    // Get the private and public keys in PEM format
+    const privateKeyPem = forge.pki.privateKeyToPem(keyPair.privateKey);
+    const publicKeyPem = forge.pki.publicKeyToPem(keyPair.publicKey);
+
+    // Output the keys
+    return { "privateKeyPem": privateKeyPem, "publicKeyPem": publicKeyPem }
+}
 
 module.exports = router
