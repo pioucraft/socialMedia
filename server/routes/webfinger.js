998697453a0b12f6e46598adcdbcf41bb367c7e1
pfannkuchen <nathan.pioucraft@outlook.com>
2023-09-22 17:27:12 +0200

    trying things

--- server/routes/webfinger.js
+++ server/routes/webfinger.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
+    console.log("request")
     try {
         let resource = req.query.resource
         if(ressource.startsWith("acct:")) {
