4e3dab08aecabcfaa08765689d2b3992372aaebf
pfannkuchen <nathan.pioucraft@outlook.com>
2023-09-22 17:05:28 +0200

    add create table

--- server/javascript/db.js
+++ server/javascript/db.js
@@ -3,6 +3,14 @@ const pg = require("pg")
 const client = new pg.Client({database: "socialmedia", user: "postgres", password: process.env.psql_password})
 client.connect().then(() => console.log("connected to the database"))
 
+try {
+    query("CREATE TABLE Users(handle VARCHAR(20), username VARCHAR(30), email VARCHAR(100), password TEXT, bio VARCHAR(1000), following TEXT, followers TEXT, profilePicture VARCHAR(36), notes TEXT, announces TEXT, likes TEXT, publicKeyPem TEXT, privateKeyPem TEXT, emailVerification TEXT, token TEXT, lastVerificationEmailSent BIGSERIAL);")
+}
+catch(err) {
+    console.log(err)
+    console.log("There's probably no error, it probably means that the tables already exist in the database.")
+}
+
 async function query(query, parameters) {
     let res
     if(parameters) {
