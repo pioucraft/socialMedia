f33084a8ae62de84917144d36ce6ee420b3984ad
pfannkuchen <nathan.pioucraft@outlook.com>
2023-09-22 17:26:12 +0200

    changing things

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import 'dotenv/config'
 import api from "./routes/api"
+import webfinger from "./routes/webfinger"
 
 const app = express()
 app.use("/api", api)
+app.use("/.well-known/webfinger", webfinger)
 
 app.listen(Number(process.env.PORT))
---
+++ server/routes/webfinger.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    try {
+        let resource = req.query.resource
+        if(ressource.startsWith("acct:")) {
+            let account = resource.split("acct:")[1].join("acct:")
+            console.log(account)
+        }
+    }
+    catch(err) {
+        res.sendStatus(500)
+    }
+})
+
+module.exports = router
