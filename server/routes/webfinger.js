d6f84b40de153b07cfab18bfdb482ebb8136b21b
pfannkuchen <nathan.pioucraft@outlook.com>
2023-09-23 07:46:23 +0200

    make it impssible ot load an account that doesn't exist

--- server/routes/webfinger.js
+++ server/routes/webfinger.js
@@ -1,13 +1,21 @@
 const express = require("express")
 const router = express.Router()
+const query = require("../javascript/db")
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     try {
         
         let resource = req.query.resource
         if(resource.startsWith("acct:")) {
             let account = resource.split("acct:")[1]
-            res.send({"subject": resource, "links": [{"rel": "self", "type": "application/activity+json", "href": `https://${process.env.URL}/users/${account}`}]})
+            let accountWithoutDomain = account.split("@")[0]
+            let accountFromDb = await query("SELECT * FROM Users WHERE handle = $1", [accountWithoutDomain]).rows[0]
+            if(accountFromDb) {
+                res.send({"subject": resource, "links": [{"rel": "self", "type": "application/activity+json", "href": `https://${process.env.URL}/users/${account}`}]})
+            }
+            else {
+                res.status(404).send("")
+            }
         }
     }
     catch(err) {
