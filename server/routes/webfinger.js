d977acdf849e39be280578dc8d546ef259b6cd80
pfannkuchen <nathan.pioucraft@outlook.com>
2023-09-23 07:53:45 +0200

    bug fix

--- server/routes/webfinger.js
+++ server/routes/webfinger.js
@@ -9,8 +9,9 @@ router.get("/", async (req, res) => {
         if(resource.startsWith("acct:")) {
             let account = resource.split("acct:")[1]
             let accountWithoutDomain = account.split("@")[0]
-            let accountFromDb = await query("SELECT * FROM Users WHERE handle = $1", [accountWithoutDomain]).rows[0]
-            if(accountFromDb) {
+            let accountDomain = account.split("@")[1]
+            let accountFromDb = (await query("SELECT * FROM Users WHERE handle = $1", [accountWithoutDomain])).rows[0]
+            if(accountFromDb && accountDomain == process.env.URL) {
                 res.send({"subject": resource, "links": [{"rel": "self", "type": "application/activity+json", "href": `https://${process.env.URL}/users/${account}`}]})
             }
             else {
