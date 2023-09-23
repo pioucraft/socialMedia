14ce170eab419cb5ce657656d5fb56585fb5e721
pfannkuchen <nathan.pioucraft@outlook.com>
2023-09-23 09:03:54 +0200

    testing users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,33 @@
 const express = require("express")
+const query = require("../javascript/db")
 
 const router = express.Router()
+
+router.get("/:handle", async (req, res) => {
+    try {
+        let handle = req.params.handle
+        let handleFromDatabse = (await query("SELECT * FROM Users WHERE handle = $1;", [handle])).rows[0]
+        if(handleFromDatabse) {
+            let response = {
+                "@context": "https://www.w3.org/ns/activitystreams",
+                "type": "Person",
+                "id": `https://${process.env.URL}/users/${handle}`,
+                "preferedUsername": handle,
+                "name": (await query("SELECT * FROM Users WHERE handle = $1;", [handle])).rows[0].username,
+                "publicKey": {
+                    "id": `https://${process.env.URL}/${handle}#main-key`,
+                    "owner": `https://${process.env.URL}/${handle}`,
+                    "publicKeyPem": (await query("SELECT * FROM Users WHERE handle = $1;", [handle])).rows[0].publickeypem
+                }
+            }
+        }
+        else {
+            res.sendStatus(404)
+        }
+    }
+    catch(err) {
+        res.sendStatus(500)
+    }
+})
 
 module.exports = router
