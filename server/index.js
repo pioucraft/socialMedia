1306c3f157cc747f6bd2ad9792c4d95e3343daa3
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-08 12:55:41 +0100

    some improvements

--- server/index.js
+++ server/index.js
@@ -1,18 +1,7 @@
-//import express from "express"
 import 'dotenv/config'
 import api from "./routes/api"
 import webfinger from "./routes/webfinger"
 import users from "./routes/users"
-
-/*const app = express()
-app.use("/api", api)
-app.use("/.well-known/webfinger", webfinger)
-app.use("/users", users)
-
-app.use("/images", express.static("./routes/api/images"))
-
-app.listen(Number(process.env.PORT))*/
-
 
 const server = Bun.serve({
     port: process.env.PORT,
