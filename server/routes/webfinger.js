330b9da55e8c64e59ea63c56e8ad581dd1469ab0
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-09 16:27:43 +0100

    test bug fix

--- server/routes/webfinger.js
+++ server/routes/webfinger.js
@@ -4,22 +4,25 @@ const query = require("../javascript/db")
 
 async function webfinger(req) {
     try {
-        let resource = req.url.split("=")[1]
-        if(resource.startsWith("acct:")) {
-            let account = resource.split("acct:")[1]
-            let accountWithoutDomain = account.split("@")[0]
-            let accountDomain = account.split("@")[1]
-            let accountFromDb = (await query("SELECT * FROM Users WHERE handle = $1", [accountWithoutDomain])).rows[0]
-            if(accountFromDb && accountDomain == process.env.DOMAIN && accountFromDb.emailverification == "yes") {
-                let response = {"subject": resource, "links": [{"rel": "self", "type": "application/activity+json", "href": `${process.env.URL}/users/${accountWithoutDomain}`}]}
-                return {"message": response, status: 200}
+        let url = unescape(req.url)
+        if(url.split("?")[1].startsWith("resource")) {
+            let resource = url.split("=")[1]
+            if(resource.startsWith("acct:")) {
+                let account = resource.split("acct:")[1]
+                let accountWithoutDomain = account.split("@")[0]
+                let accountDomain = account.split("@")[1]
+                let accountFromDb = (await query("SELECT * FROM Users WHERE handle = $1", [accountWithoutDomain])).rows[0]
+                if(accountFromDb && accountDomain == process.env.DOMAIN && accountFromDb.emailverification == "yes") {
+                    let response = {"subject": resource, "links": [{"rel": "self", "type": "application/activity+json", "href": `${process.env.URL}/users/${accountWithoutDomain}`}]}
+                    return {"message": response, status: 200}
+                }
+                else {
+                    return {"message": "404 Not Found", status: 404}
+                }
             }
             else {
                 return {"message": "404 Not Found", status: 404}
             }
-        }
-        else {
-            return {"message": "404 Not Found", status: 404}
         }
     }
     catch(err) {
