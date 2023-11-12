47a3ca005415d2617c4d2cbd6cd287556c437573
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-12 12:41:32 +0100

    test bug fix

--- server/javascript/activitystreams.js
+++ server/javascript/activitystreams.js
@@ -3,7 +3,7 @@ async function getOrderedCollection(url) {
     fetchedUrl = await fetchedUrl.json()
     console.log(fetchedUrl)
     let collection = []
-    if(fetchedUrl.first == String) {
+    if(typeof fetchedUrl.first == String) {
         let fetched = await fetch(fetchedUrl.first, {headers: {"Accept": "application/activity+json"}})
         console.log(fetched)
         //collection = 
