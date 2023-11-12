2f717ccd6f2110b53246123251dc923b0a85ede6
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-12 12:39:20 +0100

    test

--- server/javascript/activitystreams.js
+++ server/javascript/activitystreams.js
@@ -1,6 +1,13 @@
 async function getOrderedCollection(url) {
     let fetchedUrl = await fetch(url, {headers: {"Accept": "application/activity+json"}})
-    console.log(await fetchedUrl.json())
+    fetchedUrl = await fetchedUrl.json()
+    let collection = []
+    if(fetchedUrl.first == String) {
+        let fetched = await fetch(fetchedUrl.first, {headers: {"Accept": "application/activity+json"}})
+        console.log(fetched)
+        //collection = 
+        //if
+    }
 }
 
 module.exports = {"getOrderedCollection": getOrderedCollection}
