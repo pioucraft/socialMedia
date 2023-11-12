65f0829da69badf8ff58495a489cc58a78ec0b10
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-12 12:40:15 +0100

    test

--- server/javascript/activitystreams.js
+++ server/javascript/activitystreams.js
@@ -1,6 +1,7 @@
 async function getOrderedCollection(url) {
     let fetchedUrl = await fetch(url, {headers: {"Accept": "application/activity+json"}})
     fetchedUrl = await fetchedUrl.json()
+    console.log(fetchedUrl)
     let collection = []
     if(fetchedUrl.first == String) {
         let fetched = await fetch(fetchedUrl.first, {headers: {"Accept": "application/activity+json"}})
