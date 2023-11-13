5d4413c7ce86fd2ff95fa3c07699c42cd46bc865
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-13 18:10:25 +0100

    ok

--- server/javascript/encryption.js
+++ server/javascript/encryption.js
@@ -8,8 +8,14 @@ async function verifySignature(req) {
     const hash = crypto.createHash('SHA-256');
     hash.update(JSON.stringify(body), 'utf-8');
     const digest = hash.digest('base64');
+    if(digest == req.headers.get("digest").split("SHA256")[1]) {
+
+    }
+    else {
+        return false
+    }
     console.log(digest)
-    console.log(req.headers.get("digest"))
+    console.log()
 }
 
 module.exports = {"verifySignature": verifySignature}
