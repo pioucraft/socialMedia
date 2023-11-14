d66806e90a06cc0eb364b05e9b923c404f5b873f
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-14 17:27:56 +0100

    ah

--- server/javascript/encryption.js
+++ server/javascript/encryption.js
@@ -13,10 +13,30 @@ async function verifySignature(req) {
     if(digest == req.headers.get("digest").split("SHA-256=")[1]) {
         console.log("true")
         let signatureHeader = req.headers.get("signature").split(",")
-        let headers = signatureHeader[2].split('"')[1].split('"')[0]
-        console.log(headers)
+        let headersList = signatureHeader[2].split('"')[1].split('"')[0]
+        console.log(headersList)
         let signature = signatureHeader[3].split('"')[1].split('"')[0]
         console.log(signature)
+        let headers = []
+        let splitedHeaders = headersList.split(" ")
+        for(let i = 0; i<headersList.split(" ").length;i++) {
+            
+            if(splitedHeaders[i] == "(request-target)") {
+                console.log("request target")
+            }
+            else if(splitedHeaders[i] == "host") {
+                console.log("host")
+            }
+            else if(splitedHeaders[i] == "date") {
+                console.log("date")
+            }
+            else if(splitedHeaders[i] == "digest") {
+                console.log("digest")
+            }
+            else if(splitedHeaders[i] == "content-type") {
+                console.log("content-type")
+            }
+        }
     }
     else {
         return false
