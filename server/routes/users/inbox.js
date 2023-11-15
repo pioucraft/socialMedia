3f4a1422d57eb5ee07c5d6627521dad7d72e1643
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-15 17:19:22 +0100

    wtf

--- server/routes/users/inbox.js
+++ server/routes/users/inbox.js
@@ -8,7 +8,7 @@ async function inbox(req) {
         if(handleFromDatabse && req.headers.get("content-type") == "application/activity+json") {
             console.log("ah")
             if(await encryption.verifySignature(req)) {
-                let bodyw = await req.json()
+                let body = await req.json()
                 console.log("ok")
                 if(body.type == "Follow") {
                     console.log("following")
