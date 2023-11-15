439b63459909684a93b6bf706851a005dcfb17a2
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-15 13:41:25 +0100

    whyyyy

--- server/javascript/encryption.js
+++ server/javascript/encryption.js
@@ -40,7 +40,7 @@ async function verifySignature(req) {
         }
         headers = headers.join("\n")
         console.log(headers)
-        let userFetched = await (await fetch(body.actor)).json()
+        //let userFetched = await (await fetch(body.actor)).json()
         console.log(body.actor)
         //let publicKey = getUserJs.getUserAsAdmin("")
     }
