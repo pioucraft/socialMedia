79234bcf71327ae1715b1aad348bf4958ae56d18
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-15 17:24:55 +0100

    I don't understand

--- server/routes/users/inbox.js
+++ server/routes/users/inbox.js
@@ -15,7 +15,7 @@ async function inbox(req) {
                     let object = await query("SELECT * FROM Users WHERE handle = $1", [body.object.split("/")[4]])
                     console.log(object)
                 }*/
-                console.log(await req.json())
+                console.log(req.json())
             }
             else {
                 return {"message": "400 Bad Request", "status": 400}
