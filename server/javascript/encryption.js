82742f8e18769ccd6c64c1afdde46f8269b947f8
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-16 17:54:08 +0100

    whut

--- server/javascript/encryption.js
+++ server/javascript/encryption.js
@@ -76,7 +76,7 @@ async function sign(body) {
     const hash = crypto.createHash('sha256');
     hash.update(body, 'utf-8');
     const digest = hash.digest('base64');
-    console.log(body.actor)
+    console.log(body)
     let privateKey = (await query("SELECT * FROM Users WHERE handle = $1", [body.actor.split("/")[4]])).rows[0].privatekeypem
     console.log(privateKey)
     const key = crypto.createPrivateKey(privateKey)
