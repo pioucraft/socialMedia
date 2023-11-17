be280cac4371cf9ccf7a6314711218c8a674f0f9
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-17 16:16:15 +0100

    whyyyy

--- server/routes/users/inbox.js
+++ server/routes/users/inbox.js
@@ -80,7 +80,7 @@ function fetchUserInbox(url, headers) {
     return new Promise((resolve, reject) => {
       // Simulate an asynchronous task (replace with your actual logic)
       setTimeout(() => {
-        console.log(`fetch(${url}, ${JSON.stringify(headers)})`)/*.then(data => data.json()).then(data => {
+        console.log(`fetch('${url}', ${JSON.stringify(headers)})`)/*.then(data => data.json()).then(data => {
             resolve(data);
         })*/
         
