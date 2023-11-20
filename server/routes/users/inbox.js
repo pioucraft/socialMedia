64b6d811a38612ef201db2f8d5187a613285aeb4
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-20 17:51:28 +0100

    ok great

--- server/routes/users/inbox.js
+++ server/routes/users/inbox.js
@@ -104,7 +104,18 @@ async function inbox(req) {
                 }
                 else if(body.type == "Accept") {
                     if(body.object.type == "Follow") {
-                        
+                        if(await encryption.verifySignature(req, body)) {
+                            let userFromDatabase = (await query("SELECT * FROM Users WHERE handle = $1", [handle])).rows[0]
+                            let userFollowing = JSON.parse(userFromDatabase.following)
+                            for(let i=0;i<userFollowing.length;i++) {
+                                if(userFollowing[i].id == body.object.id) {
+                                    userFollowing[i].accepted = true
+                                    await query("UPDATE Users SET following = $1 WHERE handle = $2", [JSON.stringify(userFollowing), handle])
+                                    return {"message": "200 Success", "status": 200}
+                                }
+                            }
+                            
+                        }
                     }
                 }
             }
