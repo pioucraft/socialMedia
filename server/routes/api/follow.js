253af8a0461de54af7ae4e609dce33f07985b728
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-20 17:01:10 +0100

    testing

--- server/routes/api/follow.js
+++ server/routes/api/follow.js
@@ -1,6 +1,29 @@
+const query = require("../../javascript/db")
+const getUserJs = require("../../javascript/getuser")
+
+
 async function follow(req) {
     try {
+        let body = await req.json()
+        let handle = body.handle
+        let token = body.token
+        let user = body.user
+        let userFromDatabase = (await query("SELECT * FROM Users WHERE handle = $1", [handle])).rows[0]
+        let trueToken = userFromDatabase.token
+        if(trueToken == token) {
+            if(userFromDatabase.emailverification != "yes") {
+                return {"message": "401 Please Verify Your Email", "status": 401}
+            }
+            else {
+                let userFollowing = JSON.parse(userFromDatabase.following)
+                console.log(userFollowing)
+                return {"message": "Success", "status": 200}
+            }
 
+        }
+        else {
+            return {"message": "401 Unauthorized", "status": 401}
+        }
     }
     catch(err) {
         return {"message": "500 Internal Server Error", "status": 500}
