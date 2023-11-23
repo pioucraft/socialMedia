34dbc80ef66bf997eeb8814594d9b796398d94ab
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-23 19:13:49 +0100

    ok

--- server/javascript/encryption.js
+++ server/javascript/encryption.js
@@ -1,6 +1,7 @@
 const query = require("./db");
 const getUserJs = require("./getuser")
 const crypto = require("node:crypto")
+const base64 = require("base64")
 
 async function signWithoutBody(actor, rawHeaders, userLink, date) {
     let headers = rawHeaders.split(" ")
@@ -36,7 +37,7 @@ async function signWithoutBody(actor, rawHeaders, userLink, date) {
 async function verifySignature(req, body) {
     
     const hash = crypto.createHash('sha256');
-    const digest = ( await crypto.subtle.digest("SHA-256",await req.clone().arrayBuffer())).toString("base64");
+    const digest = base64.encode(await crypto.subtle.digest("SHA-256",await req.clone().arrayBuffer()));
     console.log(JSON.stringify(body))
     console.log(digest)
     console.log(req.headers.get("digest").split("SHA-256=")[1])
