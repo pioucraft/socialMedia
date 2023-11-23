2bc7c0db4d62df2801bb4b8763707e30f0adade3
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-23 19:19:49 +0100

    dumb

--- server/javascript/encryption.js
+++ server/javascript/encryption.js
@@ -37,7 +37,7 @@ async function verifySignature(req, body) {
     
     const hash = crypto.createHash('sha256');
     const encoder = new TextEncoder();
-    let digest = (await crypto.subtle.digest("SHA-256",await req.clone().arrayBuffer())).toString("base64");
+    let digest = (await crypto.subtle.digest("SHA-256",await req.clone().arrayBuffer()));
     const hashArray = Array.from(new Uint8Array(digest));
     const hashBase64 = btoa(String.fromCharCode(...hashArray))
     digest = hashBase64
