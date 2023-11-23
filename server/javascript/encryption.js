68f255d90e60fcf1d5dda403b301f6cd1c329283
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-23 19:08:15 +0100

    testing

--- server/javascript/encryption.js
+++ server/javascript/encryption.js
@@ -36,8 +36,7 @@ async function signWithoutBody(actor, rawHeaders, userLink, date) {
 async function verifySignature(req, body) {
     
     const hash = crypto.createHash('sha256');
-    hash.update(JSON.stringify(body), "utf-8");
-    const digest = hash.digest('base64');
+    const digest = await crypto.subtle.digest(await request.clone().arrayBuffer());
     console.log(JSON.stringify(body))
     console.log(digest)
     console.log(req.headers.get("digest").split("SHA-256=")[1])
