4d949ff65ff80fc2097c673d292a4b0a6d0afa1d
Nathan Gasser <github.pfannkuchen@gougoule.ch>
2023-11-24 12:38:22 +0100

    test

--- server/routes/users/inbox.js
+++ server/routes/users/inbox.js
@@ -6,7 +6,7 @@ const sanitize = require("sanitize-html")
 
 async function inbox(req) {
     try {
-        console.log(req.body)
+        console.log(await Bun.readableStreamToText(req.body))
         let body = await req.json()
         console.log(body)
         let handle = req.url.split("/")[4]
