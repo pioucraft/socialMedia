f8a701a80da573d2cef0b96e26b70527ee2d0cdd
nathan <github.pfannkuchen@gougoule.ch>
2023-12-02 10:04:18 +0100

    authentification should be working

--- server/routes/api.js
+++ server/routes/api.js
@@ -28,53 +28,53 @@ async function api(req) {
     let startOfUrlPath = `/${url.pathname.split("/")[2]}`
     console.log(startOfUrlPath)
     let body = await req.json()
-    let testedAuthentification = await testAuthentification(body)
+    let testedAuthentification = await testAuthentification(body, startOfUrlPath)
     console.log(testedAuthentification)
     if(pathsThatNeedAuthentification.includes(startOfUrlPath) && testedAuthentification != true) {
         return testedAuthentification
     }
     else if(url.pathname.startsWith("/api/createAccount")) {
-        return (await createAccount(req))
+        return (await createAccount(req, body))
     }
     else if(url.pathname.startsWith("/api/verifyEmail")) {
         return (await verifyEmail(req))
     }
     else if(url.pathname.startsWith("/api/sendVerificationEmail")) {
-        return (await sendVerificationEmail(req))
+        return (await sendVerificationEmail(req, body))
     }
     else if(url.pathname.startsWith("/api/changeUsername")) {
-        return (await changeUsername(req))
+        return (await changeUsername(req, body))
     }
     else if(url.pathname.startsWith("/api/changeEmail")) {
-        return (await changeEmail(req))
+        return (await changeEmail(req, body))
     }
     else if(url.pathname.startsWith("/api/changeBio")) {
-        return (await changeBio(req))
+        return (await changeBio(req, body))
     }
     else if(url.pathname.startsWith("/api/uploadFile")) {
-        return (await uploadFile(req))
+        return (await uploadFile(req, body))
     }
     else if(url.pathname.startsWith("/api/changePassword")) {
-        return (await changePassword(req))
+        return (await changePassword(req, body))
     }
     else if(url.pathname.startsWith("/api/login")) {
-        return (await login(req))
+        return (await login(req, body))
     }
     else if(url.pathname.startsWith("/api/changeProfilePicture")) {
-        return (await changeProfilePicture(req))
+        return (await changeProfilePicture(req, body))
     }
     else if(url.pathname.startsWith("/api/getUser")) {
-        return (await getUser(req))
+        return (await getUser(req, body))
     }
     else if(url.pathname.startsWith("/api/follow")) {
-        return (await follow(req))
+        return (await follow(req, body))
     }
     else {
         return {"message": "404 Not Found", "status": 404}
     }
 }
 
-async function testAuthentification(body) {
+async function testAuthentification(body, startOfUrlPath) {
     try {
         if(!body.password || !body.handle) {
             return {"message": "401 Missing Authentification", "status": 401}
@@ -83,10 +83,13 @@ async function testAuthentification(body) {
         let token = body.token
         let handleFromDatabase  = (await query("SELECT * FROM Users WHERE handle = $1", [handle])).rows[0]
         if(!handleFromDatabase) 
-            return {"message": "401 Unauthorized", "status": 401}
+            return {"message": "401 Incorrect Credentials", "status": 401}
         let trueToken = handleFromDatabase.token
+        let emailVerification = handleFromDatabase.emailVerification
         if(trueToken != token) 
-            return {"message": "401 Unauthorized", "status": 401}
+            return {"message": "401 Incorrect Credentials", "status": 401}
+        if(emailVerification != "yes" && startOfUrlPath != "/sendVerificationEmail") 
+            return {"message": "401 Please Verify Your Email", "status": 401}
         else 
             return true
         
--- server/routes/api/changeBio.js
+++ server/routes/api/changeBio.js
@@ -1,31 +1,19 @@
 const query = require("../../javascript/db")
 const sanitize = require("sanitize-html")
 
-async function changeBio(req) {
+async function changeBio(req, body) {
     try {
         let body = await req.json()
         let handle = body.handle
         let bio = sanitize(body.bio)
-        let token = body.token
         if(bio.length > 1000) {
             return {"message": "400 Bad Request", "status": 400}
         }
-        else {
-            let trueToken = (await query("SELECT * FROM Users WHERE handle = $1", [handle])).rows[0].token
-            if(trueToken == token) {
-                if((await query("SELECT * FROM Users WHERE handle = $1", [handle])).rows[0].emailverification != "yes") {
-                    return {"message": "401 Please Verify Your Email", "status": 401}
-                }
-                else {
-                    await query("UPDATE Users SET bio = $1 WHERE handle = $2", [bio, handle])
-                    return {"message": "Success", "status": 200}
-                }
-
-            }
-            else {
-                return {"message": "401 Unauthorized", "status": 401}
-            }
+        if((await query("SELECT * FROM Users WHERE handle = $1", [handle])).rows[0].emailverification != "yes") {
+            return {"message": "401 Please Verify Your Email", "status": 401}
         }
+        await query("UPDATE Users SET bio = $1 WHERE handle = $2", [bio, handle])
+        return {"message": "Success", "status": 200}
     }
     catch(err) {
         return {"message": "500 Internal Server Error", "status": 500}
