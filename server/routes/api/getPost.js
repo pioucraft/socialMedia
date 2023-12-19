7c2cab1ddea55ca0591a75ad2c88e5608f9c5c64
nathan <github.pfannkuchen@gougoule.ch>
2023-12-19 16:03:58 +0100

    thank you. finished

--- server/routes/api/getPost.js
+++ server/routes/api/getPost.js
@@ -2,6 +2,7 @@ const query = require("../../javascript/db")
 
 async function getPost(req, body) {
     let postUrl = req.url.split("/")[5].replaceAll("+", "/")
-    console.log(postUrl)
+    let post = query("SELECT * FROM posts WHERE link = $1", [postUrl])
+    return post
 }
 module.exports = getPost
