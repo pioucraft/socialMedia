e8b59a1aab30d2585f75a214decf3d338adb029f
nathan <github.pfannkuchen@gougoule.ch>
2023-12-19 15:45:25 +0100

    oops

--- server/routes/api/getPost.js
+++ server/routes/api/getPost.js
@@ -1,7 +1,7 @@
 const query = require("../../javascript/db")
 
 async function getPost(req, body) {
-    let postUrl = req.headers.get("url")
+    let postUrl = req.url
     console.log(postUrl)    
 }
 module.exports = getPost
