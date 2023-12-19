4543a46e4fb1295c54707fd0a567807208e261f1
nathan <github.pfannkuchen@gougoule.ch>
2023-12-19 16:05:36 +0100

    ok thanks

--- server/routes/api/getPost.js
+++ server/routes/api/getPost.js
@@ -2,7 +2,8 @@ const query = require("../../javascript/db")
 
 async function getPost(req, body) {
     let postUrl = req.url.split("/")[5].replaceAll("+", "/")
-    let post = query("SELECT * FROM remoteposts WHERE link = $1", [postUrl])
+    let post = (await query("SELECT * FROM remoteposts WHERE link = $1", [postUrl])).rows[0]
+    console.log(post)
     return post
 }
 module.exports = getPost
