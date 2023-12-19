64b9477398e62ae5973ad8a03d73f78f042de9e2
nathan <github.pfannkuchen@gougoule.ch>
2023-12-19 16:06:10 +0100

    correct return for get post

--- server/routes/api/getPost.js
+++ server/routes/api/getPost.js
@@ -4,6 +4,6 @@ async function getPost(req, body) {
     let postUrl = req.url.split("/")[5].replaceAll("+", "/")
     let post = (await query("SELECT * FROM remoteposts WHERE link = $1", [postUrl])).rows[0]
     console.log(post)
-    return post
+    return {"message": post, "status": 200}
 }
 module.exports = getPost
